196c43f21b18efceae0868feaa4040a58fb43236
YanaMazurok <yana.mazurok@oa.edu.ua>
2022-05-15 21:46:24 +0300

    Connecting libraries and preparing the basic structure of the site

--- Project22/Startup.cs
+++ Project22/Startup.cs
@@ -1,8 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -16,23 +15,28 @@ namespace Project22
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            // додаємо підтримку контроллерів (MVC)
+            services.AddControllersWithViews()
+            //виставляємо совмісність з asp.net core 3.0
+             .SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //інформація про помилки
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
             app.UseRouting();
+            //підключаємо файли css, js...
+            app.UseStaticFiles();
 
+            //реєструємо потрібні маршрути
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
